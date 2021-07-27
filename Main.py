9e1655f8e94e113f88e38fa6f86a64eb50898c26
Sedulous_Logorrhea <68473823+Sedulous-logorrhea@users.noreply.github.com>
2021-07-27 11:02:53 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,9 @@
 class Quotation:
-
-    def __init__(self, rt='', s=0, p=0, r=0, t=0, a=1000, name='', address='', cindate='', coutdate='', rno=1):
-
-        print("\n\n*****WELCOME TO Construction management System*****\n")
-
+    def __init__(self, rt='', s=0, p=0, r=0, a=1000, name='', address='', cindate='', coutdate='', rno=1001):
+        print("\n\n*****WELCOME TO Construction Management System*****\n")
         self.rt = rt
-
         self.r = r
-
-        self.t = t
-
         self.p = p
-
         self.s = s
         self.a = a
         self.name = name
@@ -23,109 +15,66 @@ class Quotation:
     def inputdata(self):
         self.name = input("\nEnter your Fullname:")
         self.address = input("\nEnter your address:")
-        self.cindate = input("\nPropsoed Enquiry Start Date:")
-        self.coutdate = input("\nProposoed Enquiry Deadline :")
+        self.cindate = input("\nProposed Enquiry Start Date:")
+        self.coutdate = input("\nProposed Enquiry Deadline :")
         print("Your Enquiry no.:", self.rno, "\n")
 
-    def Quotation_Value(self):  # sel1353
-
-        print('''We offer complete solution to all your Structual
+    def Quotation_Value(self):
+        print('''We offer complete solution to all your Structural
         Steel related construction works.''')
 
         print("1.  WARE-HOUSE CONSTRUCTION -&gt;")
-        print("2.  STRUCTURAL STEEL FABRICATION AND ERECTION -&gt;")
-        print("3.  OIL STORAGE TANK FARM CONSTRUCTION -&gt;")
-        print("4.  LUBRICANT MANUFACTURING PLANT -&gt;")
-        print("5.  OIL REFINING FACTORY -&gt;")
-        print("6.  LUBRICANT MANUFACTURING PLANT -&gt;")
+        print("2.  OIL STORAGE TANK FARM CONSTRUCTION-&gt;")
+        print("3.  LUBRICANT MANUFACTURING PLANT -&gt;")
+        print("4.  OIL REFINING FACTORY-&gt;")
+        print("5.  OIL AND GAS PIPELINES -&gt;")
 
         x = int(input("Enter the number of your choice Please-&gt;"))
 
-        n = input("Do you have a sub-contractor?:")
+        if x in (1,2,3,4,5):
+            n = input("Do you have a sub-contractor?:")
+            mes=input("Do you want to proceed with m or feet :")
+            area=int(input("Enter the area of constructables:"))        # Single . mezzanine(70%)
+            excavation=int(input("Enter the Excavation volume:"))
+            Back_filing=int(input("Enter the Back-filing volume:"))
+            R_base_f=int(input("Enter the Road base for foundation volume per ton:"))
+            Anti_termite=int(input("Enter the Anti-Termite volume:"))
+            PCC=int(input("Enter the PCC volume:"))
+            RCC_f=int(input("Enter the RCC for footing volume:"))
+            RCC_p = int(input("Enter the RCC for pedestal volume:"))
+            RCC_tb = int(input("Enter the RCC for tie beam volume:"))
+            SBW=int(input("Enter the Solid Block work volume:"))
+            BRC_mesh=int(input("Enter the Floor concrete with BRC mesh volume:"))
+            P_s=
 
-        if (x == 1):
 
-            print("you have choose room Class A")
 
-            self.s = 4000 * n
 
-        elif (x == 2):
 
-            print("you have choose room Class B")
 
-            self.s = 3000 * n
 
-        elif (x == 3):
 
-            print("you have choose room Class C")
 
-            self.s = 2000 * n
 
-        elif (x == 4):
-            print("you have choose room Class D")
 
-            self.s = 1000 * n
+
 
         else:
-
             print("please choose a work")
 
         print("Your Estimated cost of work =", self.s, "\n")
 
-    def foodpurchased(self):
-
-        print("*****RESTAURANT MENU*****")
-
-        print("1.Dessert-----&gt;100", "2.Drinks-----&gt;50", "3.Breakfast---&gt;90", "4.Lunch----&gt;110",
-              "5.Dinner---&gt;150", "6.Exit")
-
-        while (1):
-
-            c = int(input("Enter the number of your choice:"))
-
-            if (c == 1):
-                d = int(input("Enter the quantity:"))
-                self.r = self.r + 100 * d
-
-            elif (c == 2):
-                d = int(input("Enter the quantity:"))
-                self.r = self.r + 50 * d
-
-            elif (c == 3):
-                d = int(input("Enter the quantity:"))
-                self.r = self.r + 90 * d
-
-            elif (c == 4):
-                d = int(input("Enter the quantity:"))
-                self.r = self.r + 110 * d
-
-            elif (c == 5):
-                d = int(input("Enter the quantity:"))
-                self.r = self.r + 150 * d
-
-            elif (c == 6):
-                break
-            else:
-                print("You've Enter an Invalid Key")
-
-        print("Total food Cost=Rs", self.r, "\n")
-
     def display(self):
-        print("******HOTEL BILL******")
+        print("******The Quotation Summary******")
         print("Customer details:")
         print("Customer name:", self.name)
         print("Customer address:", self.address)
-        print("Check in date:", self.cindate)
-        print("Check out date", self.coutdate)
-        print("Room no.", self.rno)
-        print("Your Room rent is:", self.s)
-        print("Your Food bill is:", self.r)
+        print("Proposed contract start date:", self.cindate)
+        print("Proposed contract End date:", self.coutdate)
+        print("Enquiry no.", self.rno)
+        print("Estimated Construction value:", self.s)
 
-        self.rt = self.s + self.t + self.p + self.r
-
-        print("Your sub total Purchased is:", self.rt)
-        print("Additional Service Charges is", self.a)
-        print("Your grandtotal Purchased is:", self.rt + self.a, "\n")
+        print("Your Grand total Purchased is:", self.rt , "\n")
         self.rno += 1
 
 
@@ -135,13 +84,11 @@ def main():
     while (1):
         print("1.Enter Customer Data")
 
-        print("2.Calculate Room Rent")
+        print("2.Central Job")
 
-        print("3.Calculate Food Purchased")
+        print("3.Estimated total cost")
 
-        print("4.Show total cost")
-
-        print("5.EXIT")
+        print("4.EXIT")
 
         b = int(input("\nEnter the number of your choice:"))
         if (b == 1):
@@ -151,11 +98,8 @@ def main():
             a.Quotation_Value()
 
         if (b == 3):
-            a.foodpurchased()
-
-        if (b == 4):
             a.display()
 
-        if (b == 5):
+        if (b == 4):
             quit()
 main()
