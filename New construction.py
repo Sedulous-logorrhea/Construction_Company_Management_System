3c39a03a031550cd845d4668c7a866edcb719991
Abinraj Bhaskaran <68473823+Sedulous-logorrhea@users.noreply.github.com>
2022-01-14 08:48:33 +0400

    Delete New construction.py

--- New construction.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-    host='127.0.0.1',
-    user='root',
-    password='Sedulous')
-mycursor = mydb.cursor()
-mycursor.execute("Create database CDBM")
